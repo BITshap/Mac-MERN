3373e0a65daa391d262ff80d21588c48672913b4
Nick Shapoff <nick-shapoff@Nicks-MacBook-Pro.local>
2023-07-02 15:45:14 -0700

    working-rest

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mydb");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
