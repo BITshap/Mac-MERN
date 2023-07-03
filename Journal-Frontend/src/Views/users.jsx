22ec384c8055405be7e10b9f555a99452950f98e
Nick Shapoff <nick-shapoff@Nicks-MacBook-Pro.local>
2023-07-03 12:40:44 -0700

    Working-FrontToBack

---
+++ Journal-Frontend/src/Views/users.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Users = () => {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    // Make a GET request to your backend API
+    axios.get('http://localhost:3001/users')
+      .then(response => {
+        // Update the state with the retrieved documents
+        setDocuments(response.data);
+      })
+      .catch(error => {
+        console.error('Failed to fetch documents:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Leaderboard</h2>
+      <ul>
+        {documents.map(document => (
+          <li key={document.name}>{JSON.stringify(document.name)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/JournalMe")
-.then(()=>{
-  console.log("mongodb connected");
+//basic connection
+mongoose.connect("mongodb://localhost:27017/JournalMe", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-.catch(()=>{
-  console.log('failed');
+.then(()=>{
+  console.log("MongoDB connected");
+})
+.catch((error)=>{
+  console.error('Failed to connect to MongoDB', error);
 });
 
+
 app.use(express.static("public"));
+app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.get("/users", async (req, res) => {
+  try {
+    const db = mongoose.connection;
+    const collection = db.collection('users'); // Use your collection name here
+
+    // Find all documents in the collection
+    const documents = await collection.find().toArray();
+
+    // Send the retrieved documents as JSON response
+    res.json(documents);
+  } catch (error) {
+    console.error('Failed to retrieve documents from the database:', error);
+    res.status(500).json({ error: 'Failed to retrieve documents from the database' });
+  }
+});
+
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 });
