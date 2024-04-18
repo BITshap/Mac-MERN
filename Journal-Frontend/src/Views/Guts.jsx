ccf61ee34750d947357926ad84d04c3c655ed4fa
Nick Shapoff <nick-shapoff@Nicks-MacBook-Pro.local>
2024-04-18 11:38:44 -0700

    Gradient Reach Out

--- Journal-Frontend/src/Views/Guts.jsx
+++ Journal-Frontend/src/Views/Guts.jsx
@@ -5,9 +5,11 @@ const Guts = () => {
   return (
     <div className="container loader">
       <div className="content">
-        <h1>Keep Journaling!</h1>
-        <h1>Nick was proud of this project, and happy with his first build. He moved onto other stations, however, he is still reachable by clicking the button below.</h1>
+        <h1>Keep on Journaling! You got this!</h1>
+        <h1>Nick was very fond of this project, and it was a proud build. He moved on to other projects, however, is still reachable by clicking the space shuttle below. See you fellow travla!</h1>
         <div className="spinner-container">
+
+          
           <a href="mailto:nicholasshapoff@gmail.com" style={{ textDecoration: 'none' }}>
             <RocketSpinner />
           </a>
